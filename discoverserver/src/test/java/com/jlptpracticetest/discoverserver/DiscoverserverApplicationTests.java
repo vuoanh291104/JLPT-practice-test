a82a5ce6c7e9f6565be584155a38e56b5b5ebfdb
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2025-05-09 10:18:45 +0700

    starting project

---
+++ discoverserver/src/test/java/com/jlptpracticetest/discoverserver/DiscoverserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlptpracticetest.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

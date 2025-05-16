844987bdd0a394984979f95dcd795df4c1241533
HyuNie <huunien734@gmail.com>
2025-05-16 18:39:37 +0700

    modifier secret and add main func

--- authen-service/src/main/java/com/jlptpracticetest/authen_service/JwtUtil.kt
+++ authen-service/src/main/java/com/jlptpracticetest/authen_service/JwtUtil.kt
@@ -2,11 +2,21 @@ package com.jlptpracticetest.authen_service
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.Claims
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 
 import java.util.*
 
-object JwtUtil {
-    private const val SECRET_KEY = "your-secret-key"
+@Component
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties {
+    lateinit var secret: String
+}
+
+@Component
+class JwtUtil(@Autowired val jwtProperties: JwtProperties) {
+
     private val expiration = 1000 * 60 * 15 // 15 phút
 
     fun generateToken(data: Map<String, Any>): String {
@@ -14,10 +24,14 @@ object JwtUtil {
             .setClaims(data)
             .setIssuedAt(Date())
             .setExpiration(Date(System.currentTimeMillis() + expiration))
-            .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+            .signWith(SignatureAlgorithm.HS512, jwtProperties.secret)
             .compact()
     }
 
-    fun validateToken(token: String): Claims =
-        Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).body
+    fun validateToken(token: String): Claims {
+        return Jwts.parser()
+            .setSigningKey(jwtProperties.secret)
+            .parseClaimsJws(token)
+            .body
+    }
 }

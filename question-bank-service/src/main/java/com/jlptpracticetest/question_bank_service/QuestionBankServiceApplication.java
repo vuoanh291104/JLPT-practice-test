548ed4c3d87663487124fdd3af950105a4d0d24a
HyuNie <huunien734@gmail.com>
2025-05-22 23:33:55 +0700

    fix undefine from inport

--- question-bank-service/src/main/java/com/jlptpracticetest/question_bank_service/QuestionBankServiceApplication.java
+++ question-bank-service/src/main/java/com/jlptpracticetest/question_bank_service/QuestionBankServiceApplication.java
@@ -1,10 +1,8 @@
 package com.jlptpracticetest.question_bank_service;
 
 
-import com.jlptpracticetest.exam_service.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 

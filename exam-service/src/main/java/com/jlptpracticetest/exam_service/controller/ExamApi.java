c75334ec65573e3e6114233c98a936870a78f991
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2025-05-23 18:04:12 +0700

    add CORS

--- exam-service/src/main/java/com/jlptpracticetest/exam_service/controller/ExamApi.java
+++ exam-service/src/main/java/com/jlptpracticetest/exam_service/controller/ExamApi.java
@@ -3,6 +3,7 @@ package com.jlptpracticetest.exam_service.controller;
 import com.jlptpracticetest.exam_service.dto.ExamDTO;
 import com.jlptpracticetest.exam_service.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ public class ExamApi {
     @Autowired
     private ExamService examService;
     @GetMapping("")
-    public List<ExamDTO> getExamByLevel (@RequestParam String level){
-        return examService.getExamByLevel(level);
+    public ResponseEntity<List<ExamDTO>> getExamByLevel (@RequestParam String level){
+        return ResponseEntity.ok(examService.getExamByLevel(level));
     }
 }
--- exam-service/src/main/java/com/jlptpracticetest/exam_service/jwt/JwtUtil.kt
+++ exam-service/src/main/java/com/jlptpracticetest/exam_service/jwt/JwtUtil.kt
@@ -1,16 +1,16 @@
-package com.jlptpracticetest.exam_service.jwt
-
-import org.springframework.stereotype.Component
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.Claims
-
-@Component
-class JwtUtil(val jwtProperties: JwtProperties) {
-
-    fun validateToken(token: String): Claims {
-        return Jwts.parser()
-            .setSigningKey(jwtProperties.secret)
-            .parseClaimsJws(token)
-            .body
-    }
-}
+//package com.jlptpracticetest.exam_service.jwt
+//
+//import org.springframework.stereotype.Component
+//import io.jsonwebtoken.Jwts
+//import io.jsonwebtoken.Claims
+//
+//@Component
+//class JwtUtil(val jwtProperties: JwtProperties) {
+//
+//    fun validateToken(token: String): Claims {
+//        return Jwts.parser()
+//            .setSigningKey(jwtProperties.secret)
+//            .parseClaimsJws(token)
+//            .body
+//    }
+//}
